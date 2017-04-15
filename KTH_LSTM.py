d0ee760110aa93b2715e6ad8585de0a9bfad4454
Chamin Morikawa <chamin@kusunoki>
2017-04-16 05:50:06 +0900

    Updated comments for readability.

--- KTH_LSTM.py
+++ KTH_LSTM.py
@@ -1,7 +1,7 @@
 '''
 This program demonstrates how to train and use a deep neural network with
 2D Convolutional and recurrent layers, for activity recognition in videos.
-I selected the KTH dataset since it is relatively small and has activities 
+I selected the KTH dataset since it is relatively small and has activities
 that are easy to learn.
 
 Chamin Morikawa
@@ -31,16 +31,16 @@ import re
 _nsre = re.compile('([0-9]+)')
 def natural_sort_key(s):
     return [int(text) if text.isdigit() else text.lower()
-            for text in re.split(_nsre, s)]   
+            for text in re.split(_nsre, s)]
 
 # use this if you want reproducibility
-#np.random.seed(2016)  
+#np.random.seed(2016)
 
 # we will be using tensorflow
 K.set_image_dim_ordering('tf')
 
 # specifiy the path to your KTH data folder
-trg_data_root = "/nfs/trg_data/master/public/KTH/"
+trg_data_root = "/path/to/dataset/KTH/"
 
 # load training or validation data
 # with 25 persons in the dataset, start_index and finish_index has to be in the range [1..25]
@@ -50,36 +50,36 @@ def load_data_for_persons(root_folder, start_index, finish_index, frames_per_cli
     frame_path = "/frames/"
     frame_set_prefix = "person" # 2 digit person ID [01..25] follows
     rec_prefix = "d" # seq ID [1..4] follows
-    rec_count = 4 
+    rec_count = 4
     seg_prefix = "seg" # seq ID [1..4] follows
-    seg_count = 4 
-    
+    seg_count = 4
+
     data_array = []
     classes_array = []
-    
+
     # let's make a couple of loops to generate all of them
-    for i in range(0, len(class_labels)):    
+    for i in range(0, len(class_labels)):
         # class
         class_folder = trg_data_root + class_labels[i] + frame_path
-        
+
         for j in range(start_index, finish_index+1):
             # person
             if j<10:
                 person_folder = class_folder + frame_set_prefix + "0" + str(j) + "_" + class_labels[i] + "_"
             else:
                 person_folder = class_folder + frame_set_prefix + str(j) + "_" + class_labels[i] + "_"
-            
+
             for k in range(1,rec_count+1):
                 # recording
                 rec_folder = person_folder + rec_prefix + str(k) + "/"
                 for m in range(1,seg_count+1):
                     # segment
                     seg_folder = rec_folder + seg_prefix + str(m) + "/"
-                
+
                     # get the list of files
-                    file_list = [f for f in os.listdir(seg_folder)]                
+                    file_list = [f for f in os.listdir(seg_folder)]
                     example_size = len(file_list)
-                
+
                     # for larger segments, we can change the starting point to augment the data
                     clip_start_index = 0
                     if example_size > frames_per_clip:
@@ -87,32 +87,32 @@ def load_data_for_persons(root_folder, start_index, finish_index, frames_per_cli
                         #clip_start_index = randint(0, (example_size - frames_per_clip))
                         # sample the frames from the center
                         clip_start_index = example_size/2 - frames_per_clip/2
-                        example_size = frames_per_clip 
-                        
+                        example_size = frames_per_clip
+
                     # need natural sort before loading data
                     file_list.sort(key=natural_sort_key)
-                
+
                     #create a list for each segment
                     current_seg_temp = []
                     for n in range(clip_start_index,example_size+clip_start_index):
                         file_path = seg_folder + file_list[i]
-                        data = np.asarray( Image.open( file_path), dtype='uint8' ) 
+                        data = np.asarray( Image.open( file_path), dtype='uint8' )
                         # remove unnecessary channels
                         data_gray = np.delete(data,[1,2],2)
                         data_gray = data_gray.astype('float32')/255.0
                         current_seg_temp.append(data_gray)
-                
+
                     # preprocessing
                     current_seg = np.asarray(current_seg_temp)
                     current_seg = current_seg.astype('float32')
-                    
+
                     data_array.append(current_seg)
                     classes_array.append(i)
-                    
+
     # # create one-hot vectors from output values
     classes_one_hot = np.zeros((len(classes_array), len(class_labels)))
     classes_one_hot[np.arange(len(classes_array)), classes_array] = 1
-    
+
     # done
     return (np.array(data_array), classes_one_hot)
 
@@ -128,7 +128,7 @@ print("Building model")
 
 #define our time-distributed setup
 model = Sequential()
-    
+
 # note: the architecture below is just for demonstration.
 # for higher accuracy, you will have to change the number and dimensions of layers.
 
@@ -151,7 +151,7 @@ model.add(TimeDistributed(Flatten()))
 # a single dense layer
 model.add(TimeDistributed(Dense(80)))
 model.add(BatchNormalization()) # required for ensuring that the network learns
-model.add(Activation('relu')) 
+model.add(Activation('relu'))
 
 # GRU layers
 #model.add(GRU(output_dim=100,return_sequences=True))
@@ -174,27 +174,28 @@ batch_size = 16 # increase if your system can cope with more data
 nb_epochs = 12 # I once achieved 77.5% accuracy with 100 epochs. Feel free to change
 
 print ("Loading data")
-# load training data: I could not load all, so I selected 1-7
-X_train, y_train = load_data_for_persons(trg_data_root, 1, 8, maxToAdd)
-# NOTE: using DataGenerator should allow loading much larger datasets
-# will update the code when I find out how to do that
+# load training data
+X_train, y_train = load_data_for_persons(trg_data_root, 1, 25, maxToAdd)
+# NOTE: if you can't fit all data in memory, load a few users at a time and
+# use multiple epochs. I don't recommend using one user at a time, since
+# it prevents good shuffling.
 
 # perform training
 print("Training")
-model.fit(np.array(X_train), y_train, batch_size=batch_size, nb_epoch=nb_epochs, shuffle=True, verbose=1)     
-    
+model.fit(np.array(X_train), y_train, batch_size=batch_size, nb_epoch=nb_epochs, shuffle=True, verbose=1)
+
 # clean up the memory
 X_train       = None
-y_train       = None 
+y_train       = None
 X_val = None
 y_val = None
 gc.collect()
 
-print("Testing") 
+print("Testing")
 
 # load test data: in this case, person 9
 X_test, y_test = load_data_for_persons(trg_data_root, 9, 9, maxToAdd)
-print('Total no. of testing samples used:', y_test.shape[0])   
+print('Total no. of testing samples used:', y_test.shape[0])
 
 preds = model.predict(np.array(X_test))
 
@@ -203,19 +204,19 @@ accurate_count = 0.0
 for i in range(0,len(preds)):
     # updating confusion matrix
     confusion_matrix[np.argmax(preds[i])][np.argmax(np.array(y_test[i]))] += 1
-    
+
     # if you are not sure of the axes of the confusion matrix, try the following line
     #print ('Predicted: ', np.argmax(preds[i]), ', actual: ', np.argmax(np.array(y_val_one_hot[i])))
-    
-    # calculating overall accuracy    
+
+    # calculating overall accuracy
     if np.argmax(preds[i])==np.argmax(np.array(y_test[i])):
         accurate_count += 1
-    
+
 print('Validation accuracy: ', 100*accurate_count/len(preds)),' %'
 print('Confusion matrix:')
 print(class_labels)
 print(confusion_matrix)
-    
+
 #save the model
 jsonstring  = model.to_json()
 with open("KTH_LSTM.json",'wb') as f:
